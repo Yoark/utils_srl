2de29a8982266cb0cd24baef6e8d1c88eaf2d1bc
Yoark <yoark.yang@gmail.com>
2020-03-10 01:17:16 -0600

    dataloader

--- bound_image_srl.py
+++ bound_image_srl.py
@@ -62,6 +62,11 @@ class jsonSrlReader(SrlReader):
 
 @DatasetReader.register("bound_image_srl")
 class BoundSrlReader(SrlReader):
+
+    def __init__(self, token_indexers=None, domain_identifier=None, lazy=False, bert_model_name=None):
+        super().__init__(token_indexers=token_indexers, domain_identifier=domain_identifier, lazy=lazy, bert_model_name=bert_model_name)
+        self.imgid2img = {}
+
     
     def _read(self, folder_path):
         #file_path = cached_path(text_path)
@@ -70,39 +75,27 @@ class BoundSrlReader(SrlReader):
         img_file = folder_path + 'imgs.tsv'
         #if img_path:
         img_features = load_obj_tsv(img_file)
+        for img_datum in img_features:
+            self.imgid2img[img_datum['img_id']] = img_datum
 
         with open(text_file) as f:
             quples = json.loads(f.read())
             for srl in quples:
                 tokens = [Token(t) for t in srl["caption"].split()]
-                img_id = srl["image"]
                 tags = srl["tag"].split()
-                verb_indicator = [0] * len(tags)
-                verb_indicator["predicate"] = 1
+                # verb_indicator = [0] * len(tags)
+                # verb_indicator["predicate"] = 1
+                verb_indicator = [1 if label[-2:] == "-V" else 0 for label in tags]
 
-            for i, line in enumerate(f.readlines()):
-                srl = json.loads(line)
-                tokens = [Token(t) for t in srl["words"]]
-                # ? maybe put it somewhere else
-                img_id = i // 5
-                img_emb = img_embs[img_id]
-                if not srl['verbs']:
-                # Sentence contains no predicates.
-                    tags = ["O" for _ in tokens]
-                    verb_label = [0 for _ in tokens]
-                    yield self.text_to_instance(tokens, verb_label, img_emb, tags)
-                else:
-                    for verb_dict in srl['verbs']:
-                        verb_indicator = [1 if label[-2:] == "-V" else 0 for label in verb_dict['tags']]
-                        tags = verb_dict['tags']
-                        #print(img_emb.shape)
-                        #import ipdb; ipdb.set_trace()
-                        yield self.text_to_instance(tokens, verb_indicator, img_emb, tags)
+                # remove .jpg
+                img = self.imgid2img[srl["image"][:-4]]
+
+                yield self.text_to_instance(tokens, verb_indicator, img, tags)
 
 
 
     def text_to_instance(  # type: ignore
-        self, tokens: List[Token], verb_label: List[int], image_imb, tags: List[str] = None
+        self, tokens: List[Token], verb_label: List[int], img, tags: List[str] = None
     ) -> Instance:
         """
         We take `pre-tokenized` input here, along with a verb label.  The verb label should be a
@@ -128,9 +121,22 @@ class BoundSrlReader(SrlReader):
         else:
             text_field = TextField(tokens, token_indexers=self._token_indexers)
             verb_indicator = SequenceLabelField(verb_label, text_field)
-            image_imb = torch.from_numpy(image_imb)
             #? Maybe other options???
-            img_field = ArrayField(image_imb)
+            img_feats = img['features'].copy()
+            img_boxes = img['boxes'].copy()
+            obj_num = img['num_boxes']
+            assert len(img_feats) == len(img_boxes) == obj_num
+
+            # Normalize the boxes to 0 ~ 1
+            img_boxes = img_boxes.copy()
+            img_boxes[:, (0, 2)] /= img['img_w']
+            img_boxes[:, (1, 3)] /= img['img_h']
+            np.testing.assert_array_less(img_boxes, 1+1e-5)
+            np.testing.assert_array_less(-img_boxes, 0+1e-5)
+
+        # Concat box feats to each object features
+        img_concat = np.hstack((img_feats, img_boxes))
+        img_field = ArrayField(img_concat)
 
         fields: Dict[str, Field] = {}
         fields["tokens"] = text_field
@@ -147,6 +153,7 @@ class BoundSrlReader(SrlReader):
         metadata_dict["words"] = [x.text for x in tokens]
         metadata_dict["verb"] = verb
         metadata_dict["verb_index"] = verb_index
+        # metadata_dict["tags"] = tags
 
         if tags:
             if self.bert_tokenizer is not None:
